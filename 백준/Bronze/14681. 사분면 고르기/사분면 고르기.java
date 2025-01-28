e9b3cf8d34812d4d3fd9c323e040a54f517048d3
DOGYUNKIM <128977556+SexyDoGyun@users.noreply.github.com>
2025-01-28 13:14:06 +0900

    [Bronze V] Title: 사분면 고르기, Time: 180 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -3,22 +3,18 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        
-        if(x>0){
-            if(y>0){
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        if(A>0){
+            if(B>0){
                 System.out.println("1");
-            }
-            else if(y<0){
+            } else if(B<0){
                 System.out.println("4");
             }
-        }
-        else if(x<0){
-            if(y>0){
+        } else if(A<0){
+            if (B>0){
                 System.out.println("2");
-            }
-            else if(y<0){
+            } else if(B<0){
                 System.out.println("3");
             }
         }
