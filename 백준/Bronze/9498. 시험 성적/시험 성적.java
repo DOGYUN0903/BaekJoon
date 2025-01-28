7e74d1841245442a6918a3a57566b454c6edeb52
DOGYUNKIM <128977556+SexyDoGyun@users.noreply.github.com>
2025-01-28 12:53:58 +0900

    [Bronze V] Title: 시험 성적, Time: 180 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -4,20 +4,15 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
-        
         if(A>=90){
             System.out.println("A");
-        }
-        else if(A>=80){
+        } else if(A>=80){
             System.out.println("B");
-        }
-        else if(A>=70){
+        } else if(A>=70){
             System.out.println("C");
-        }
-        else if(A>=60){
+        } else if(A>=60){
             System.out.println("D");
-        }
-        else{
+        } else {
             System.out.println("F");
         }
     }
