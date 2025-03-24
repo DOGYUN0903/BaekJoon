2a5e8091a09e528c15d39c9056d345a92077dd56
DOGYUNKIM <128977556+SexyDoGyun@users.noreply.github.com>
2025-03-24 17:09:13 +0900

    [Bronze III] Title: 최댓값, Time: 192 ms, Memory: 18608 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,34 +1,31 @@
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int[][] arr = new int[9][9];
-
-        // 배열 입력
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
                 arr[i][j] = sc.nextInt();
             }
         }
-
-        // 최댓값 찾기
+        
         int max = -1;
-        int row = 0;
-        int col = 0;
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (arr[i][j] > max) {
-                    max = arr[i][j];
-                    row = i;
-                    col = j;
-                }
+        int maxCol = 0;
+        int rowCol = 0;
+        
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+             if(arr[i][j] > max){
+                 max = arr[i][j];
+                 maxCol = i;
+                 rowCol = j;
+             }   
             }
         }
-
-        // 출력
+        
         System.out.println(max);
-        System.out.println((row + 1) + " " + (col + 1)); // 1-based 인덱스
+        System.out.println((maxCol+1)+ " " + (rowCol+1));
     }
 }
