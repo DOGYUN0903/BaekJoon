c9079738b8340020ea4ad3f788bb28a0f375e089
DOGYUNKIM <128977556+SexyDoGyun@users.noreply.github.com>
2025-04-02 13:09:23 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.51 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();
+
+        char[] chars = my_string.toCharArray();
+
+        Arrays.sort(chars);
+        
+        String answer = new String(chars);
+        return answer;
+    }
+}
