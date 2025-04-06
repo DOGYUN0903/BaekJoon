6e3debc75fea44ea4561b5f401f6764b2ba040d9
DOGYUNKIM <128977556+SexyDoGyun@users.noreply.github.com>
2025-04-06 13:51:46 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int n) {
-        return lcm(n, 6) / 6;
-    }
-    
-    private int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
-    }
-    
-    private int lcm(int a, int b) {
-        return (a * b) / gcd(a, b);
+        int answer = 1;
+        while(true){
+            if((answer*6) % n == 0){
+                return answer;
+            }
+            answer++;
+        }
     }
 }
