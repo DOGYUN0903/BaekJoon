50c100dc8c78b43c4d78934413e2d3993a18b6b9
DOGYUNKIM <128977556+SexyDoGyun@users.noreply.github.com>
2025-04-12 00:51:34 +0900

    [Bronze II] Title: 숫자의 개수, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int sum = A*B*C;
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+        String num = sum + "";
+        char[] ch = num.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            int digit = ch[i] - '0'; 
+            arr[digit]++;             
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
