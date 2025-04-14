f4e95a7b68121b47b07605ffaf576b5537687e49
DOGYUNKIM <128977556+SexyDoGyun@users.noreply.github.com>
2025-04-14 09:44:11 +0900

    [Bronze II] Title: 음계, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean isAscending = true;
+        boolean isDescending = true;
+
+        for (int i = 0; i < 7; i++) {
+            if (arr[i] < arr[i + 1]) {
+                isDescending = false;
+            } else if (arr[i] > arr[i + 1]) {
+                isAscending = false;
+            }
+        }
+
+        if (isAscending) {
+            System.out.println("ascending");
+        } else if (isDescending) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+    }
+}
