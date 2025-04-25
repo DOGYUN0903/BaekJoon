210b9af0c37d47e8bb528f01897f2b43afca2c0e
DOGYUNKIM <128977556+SexyDoGyun@users.noreply.github.com>
2025-04-25 09:24:08 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,9 +3,10 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(n>0){
-            answer += n%10;
-            n /= 10;
+        String a = String.valueOf(n);
+        char[] array = a.toCharArray();
+        for(int i = 0; i < array.length; i++){
+            answer += array[i] - '0';
         }
 
         return answer;
