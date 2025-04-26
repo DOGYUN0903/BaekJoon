a28da3d8769eaa6bc47a6087ae687d898845b9c8
DOGYUNKIM <128977556+DOGYUN0903@users.noreply.github.com>
2025-04-27 00:37:18 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
-        String num = String.valueOf(x);
-        char[] ch = num.toCharArray();
+        String str = String.valueOf(x);
+        char[] ch = str.toCharArray();
         int sum = 0;
-        for(int i = 0; i < ch.length; i ++){
-            sum += ch[i] -'0';
+        for(int i = 0; i < ch.length; i++){
+            sum += ch[i] - '0';
         }
-        if (x % sum == 0){
+        if(x % sum == 0){
             answer = true;
         } else{
             answer = false;
