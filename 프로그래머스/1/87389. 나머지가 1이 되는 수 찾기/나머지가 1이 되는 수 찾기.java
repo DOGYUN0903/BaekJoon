f74fd89bf5e3a03d94bc0623dfe0ebda2dfb5047
DOGYUNKIM <128977556+DOGYUN0903@users.noreply.github.com>
2025-05-01 19:18:36 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 10.73 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -3,18 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>(); // 나머지가 1이 되는 수가 몇개가 나올지 모르니까 List 사용
         for(int i = 1; i <= n; i++){
             if (n % i == 1){
                 list.add(i);
             }
         }
-        int[] arr = new int[list.size()];
+        int[] arr = new int[list.size()]; // 리스트의 사이즈로 배열의 크기 파악 가능
         for(int j = 0; j < list.size(); j++){
             arr[j] = list.get(j);
         }
-        Arrays.sort(arr);
-        answer = arr[0];
+        Arrays.sort(arr); //배열을 오름차순으로 정렬해줍니다.
+        answer = arr[0]; // 맨 처음 값이 최솟값이다.
         return answer;
     }
 }
