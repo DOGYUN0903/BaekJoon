d9379c806505b61a0b22a0ee8559c40d9c8ce0a7
DOGYUNKIM <kimdk0903@naver.com>
2025-06-23 09:39:14 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(int n) {
-    for (int i = 1; i * i <= n; i++) {
-        if (i * i == n) {
-            return 1;
-        }
+        return Math.sqrt(n) % 1 == 0 ? 1 : 2;
     }
-    return 2;
-}
-
 }
