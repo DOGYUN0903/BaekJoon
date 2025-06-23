6b1e946c4e1bfbcf55a3efbd600d7b76f778339a
DOGYUNKIM <kimdk0903@naver.com>
2025-06-23 09:47:02 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,19 +1,13 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
-        int count1 = 0;
-        int count2 = 0;
         for(int i = 0; i < num_list.length; i++){
-            if (num_list[i] % 2 == 0 ){
-                count2++;
-            } else{
-                count1++;
+            if (num_list[i] % 2 == 0){
+                answer[0]++;
+            } else {
+                answer[1]++;
             }
         }
-        
-        answer[0] = count2;
-        answer[1] = count1;
-        
         return answer;
     }
 }
