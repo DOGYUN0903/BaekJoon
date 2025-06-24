02b9cc19919f5a059a30eaafae080e37db3296a3
DOGYUNKIM <kimdk0903@naver.com>
2025-06-24 09:53:32 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,8 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2-num1+1];
-        for(int i = 0; i<answer.length; i++){
-            answer[i] = numbers[num1+i];
+        List<Integer> lists = new ArrayList<>();
+        for (int i = num1; i <= num2; i++){
+            lists.add(numbers[i]);
+        }
+        int[] answer = new int[lists.size()];
+        for (int i = 0; i < answer.length; i++){
+            answer[i] = lists.get(i);
         }
         return answer;
     }
