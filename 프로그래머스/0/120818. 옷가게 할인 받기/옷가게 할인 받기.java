c8abbb9f62b5b72a90dc04f712ef06d27fbbad24
DOGYUNKIM <kimdk0903@naver.com>
2025-06-24 11:14:34 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int price) {
         int answer = 0;
-        if (price >= 500000) {
-            answer = price * 80 / 100;
-        } else if (price >= 300000) {
-            answer = price * 90 / 100;
-        } else if (price >= 100000) {
-            answer = price * 95 / 100;
-        } else{
-            answer = price;
-        }
+        if (price < 100000) answer = price;
+        if (price >= 100000 && price < 300000) answer = (int)(price * 0.95);
+        if (price >= 300000 && price < 500000) answer = (int)(price * 0.9);
+        if (price >= 500000) answer = (int)(price * 0.8);
         return answer;
     }
 }
