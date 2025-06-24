e2b0143cd06bd723282f8f135409c7defcde6ca1
DOGYUNKIM <kimdk0903@naver.com>
2025-06-24 10:01:32 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,11 +1,17 @@
 class Solution {
     public int solution(int slice, int n) {
         int answer = 0;
-        if(n%slice == 0){
-            answer = n/slice;
-        } else{
-            answer = n/slice+1;
+        if (n % slice == 0) {
+            answer = n / slice;
+        } else {
+            if (n > slice) {
+            answer = n / slice + 1;
         }
+        if (n < slice) {
+            answer = 1;
+        }
+        }
+        
         return answer;
     }
 }
