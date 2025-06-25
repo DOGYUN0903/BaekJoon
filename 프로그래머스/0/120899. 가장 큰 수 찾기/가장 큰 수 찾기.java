db37082319f1b5b72229bbf7a739607b9d4ccd7e
DOGYUNKIM <kimdk0903@naver.com>
2025-06-25 10:35:20 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] solution(int[] array) {
-        int max = array[0];
         int index = 0;
-
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++){
+            if (max < array[i]){
                 max = array[i];
                 index = i;
             }
         }
-
-        return new int[] { max, index };
+        
+        return new int[]{max, index};
     }
 }
