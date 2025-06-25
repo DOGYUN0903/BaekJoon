49284d4d6b1e74246c3735f3f0711508ca33e047
DOGYUNKIM <kimdk0903@naver.com>
2025-06-25 09:31:28 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.38 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -5,15 +5,9 @@ class Solution {
         Arrays.sort(numbers);
         
         int n = numbers.length;
-        int case1 = numbers[n - 1] * numbers[n - 2];
-        int case2 = numbers[0] * numbers[1];
-
-        int answer;
-        if (case1 > case2) {
-            answer = case1;
-        } else {
-            answer = case2;
-        }
-        return answer;
+        int max1 = numbers[0] * numbers[1];           
+        int max2 = numbers[n - 1] * numbers[n - 2];  
+        
+        return Math.max(max1, max2);
     }
 }
