fcc003b911e931f473d0e2ffc8fa8b851d7b3de4
DOGYUNKIM <kimdk0903@naver.com>
2025-06-27 09:53:01 +0900

    [level 0] Title: 부분 문자열, Time: 0.05 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        if(str2.contains(str1)){
-            return 1;
-        }
-        return answer;
+        return str2.contains(str1) ? 1 : 0;
     }
 }
