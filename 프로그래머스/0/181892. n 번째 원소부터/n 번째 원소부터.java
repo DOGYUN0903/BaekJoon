c47c025080f283cafe6731c3130f84c16f7b5949
DOGYUNKIM <kimdk0903@naver.com>
2025-06-28 13:39:18 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.05 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> list = new ArrayList<>();
-    
-        for(int i = n - 1; i < num_list.length; i++){
-            list.add(num_list[i]);
+        List<Integer> lists = new ArrayList<>();
+        for (int i = n - 1; i < num_list.length; i++){
+            lists.add(num_list[i]);
         }
-        int[] answer = new int[list.size()];
-        for(int j = 0; j < answer.length; j++){
-            answer[j] = list.get(j);
+        
+        int[] answer = new int[lists.size()];
+        for (int i = 0; i < answer.length; i++){
+            answer[i] = lists.get(i);
         }
         return answer;
     }
