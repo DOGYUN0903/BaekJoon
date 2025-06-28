77159f36b3b27dddd721d07338629315efbdb98f
DOGYUNKIM <kimdk0903@naver.com>
2025-06-29 01:18:29 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        List<Integer> list = new ArrayList<>();
-        for(int i = start_num; i <= end_num; i++){
-            list.add(i);
+        List<Integer> lists = new ArrayList<>();
+        for (int i = start_num; i <= end_num; i++){
+            lists.add(i);
         }
-        
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = list.get(i);
+        int[] answer = new int[lists.size()];
+        for (int i = 0; i < answer.length; i++){
+            answer[i] = lists.get(i);
         }
         return answer;
     }
