b22276a295ed6dfc6aef80811842afdf15d21cb2
DOGYUNKIM <kimdk0903@naver.com>
2025-07-01 10:23:19 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        for(int i = 0; i < absolutes.length; i++){
-            if(signs[i] == true){
+        for (int i = 0; i < signs.length; i++){
+            if (signs[i] == true){
                 answer += absolutes[i];
-            } else{
+            } else {
                 answer -= absolutes[i];
             }
         }
