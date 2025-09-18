e27a016e5bea860bd94c2234ce26987aa0a2ce9e
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 21:29:09 +0900

    [level 0] Title: 자릿수 더하기, Time: 7.60 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,12 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        while (n > 0){
-            answer += n % 10;
-            n = n / 10;
+        String nums = n + "";
+        char[] chars = nums.toCharArray();
+        int sum = 0;
+        for(int i = 0; i < chars.length; i++){
+            sum += chars[i] - '0';
         }
-        
-        return answer;
+        return sum;
     }
 }
