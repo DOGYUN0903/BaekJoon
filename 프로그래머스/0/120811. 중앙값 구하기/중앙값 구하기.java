63682e97ec27c98c728fd258c4111e0bd9cd7955
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 22:53:52 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.58 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,8 +1,8 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
         Arrays.sort(array);
-        return array[array.length/2];
+        return array[array.length / 2];
     }
 }
