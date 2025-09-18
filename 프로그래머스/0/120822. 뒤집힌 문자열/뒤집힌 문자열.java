015091ff07fe42df04266105b9745623bf3e8714
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 20:46:41 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,10 +1,7 @@
 class Solution {
     public String solution(String my_string) {
-        char[] strings = my_string.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < strings.length; i++){
-            sb.append(strings[strings.length - i - 1]);
-        }
-        return sb.toString();
+        sb.append(my_string);
+        return sb.reverse().toString();
     }
 }
