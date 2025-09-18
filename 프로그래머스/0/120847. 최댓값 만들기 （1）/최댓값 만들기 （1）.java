e6c136a59eaf8b6f64d9acf1f308cc660c2f8a65
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 21:23:47 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.60 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -3,6 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
         Arrays.sort(numbers);
-        return numbers[numbers.length -1] * numbers[numbers.length -2];
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
