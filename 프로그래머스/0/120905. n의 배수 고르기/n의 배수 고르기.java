564ffb4eb2a56b4ed1226afcecc8eb1e7e7487cd
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 22:01:51 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -3,15 +3,14 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, int[] numlist) {
         List<Integer> lists = new ArrayList<>();
-        for(int i = 0; i < numlist.length; i++){
+        for (int i = 0; i < numlist.length; i++){
             if (numlist[i] % n == 0){
                 lists.add(numlist[i]);
             }
         }
-        
         int[] answer = new int[lists.size()];
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = lists.get(i);
+        for(int j = 0; j < answer.length; j++){
+            answer[j] = lists.get(j);
         }
         return answer;
     }
