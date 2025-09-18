91d84622302628c25adbbbc9d153b0f6a69bd773
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 22:23:36 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,10 +1,10 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string;
-        String[] vowels = {"a","e","i","o","u"};
-        for(String v : vowels){
-            answer = answer.replaceAll(v,"");
-        }
-        return answer;
+        String answer1 = my_string.replace("a", "");
+        String answer2 = answer1.replace("e", "");
+        String answer3 = answer2.replace("i", "");
+        String answer4 = answer3.replace("o", "");
+        String answer5 = answer4.replace("u", "");        
+        return answer5;
     }
 }
