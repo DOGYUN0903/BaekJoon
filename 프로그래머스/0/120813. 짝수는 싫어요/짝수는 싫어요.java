754acb6fadc4b881a08624f80b20f36d8cde4f91
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 22:57:10 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.08 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,17 +3,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int n) {
         List<Integer> lists = new ArrayList<>();
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++){
             if (i % 2 != 0){
                 lists.add(i);
             }
         }
+        
         int[] answer = new int[lists.size()];
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = lists.get(i);
+        for (int j = 0; j < answer.length; j++){
+            answer[j] = lists.get(j);
         }
         
-        Arrays.sort(answer);
         return answer;
     }
 }
