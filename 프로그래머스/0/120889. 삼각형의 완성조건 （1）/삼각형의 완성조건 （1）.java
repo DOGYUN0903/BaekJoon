511c0a472d88e97a3577447172436218ec5b2c20
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 21:33:23 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.59 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -2,13 +2,9 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] sides) {
-        int answer = 0;
         Arrays.sort(sides);
-        if (sides[2] < sides[0] + sides[1]){
-            answer = 1;
-        } else {
-            answer = 2;
-        }
-        return answer;
+        int sum = sides[0] + sides[1];
+        if (sum > sides[2]) return 1;
+        return 2;
     }
 }
