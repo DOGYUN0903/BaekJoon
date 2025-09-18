f0c56740036f6290e5cc29a20453058d43d46aa4
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 22:51:33 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
         for(int i = 0; i < answer.length; i++){
-            answer[i] = numbers[i] * 2;
+            answer[i] = 2 * numbers[i];
         }
         return answer;
     }
