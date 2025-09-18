d584fd7407eacefbd2a73c8c52a92181d53ead84
DOGYUNKIM <kimdk0903@naver.com>
2025-09-18 20:34:52 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        if (n < 10) {
-            answer = 12000 * n + 2000 * k;
-        } else {
-            answer = 12000 * n + 2000 * k - 2000 * (n / 10);
-        }
-        return answer;
+        if (n < 10) return 12_000*n + 2_000*k;
+        return 12_000*n + 2_000*k - n/10*2000;
     }
 }
