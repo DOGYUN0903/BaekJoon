e6f803934437eadd949cf8261c8b7369f43781a1
DOGYUNKIM <kimdk0903@naver.com>
2025-09-19 12:43:56 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,13 +1,11 @@
 class Solution {
     public String solution(int age) {
-        // a = 0, b = 1, c = 2
-        String answer = "";
-        String strAge = String.valueOf(age);
-        char[] ch = strAge.toCharArray();
-        for (int i = 0; i < ch.length; i++){
-            int num = ch[i] - '0';
-            answer += (char) ('a'+ num);
+        StringBuilder sb = new StringBuilder();
+        char[] ch = String.valueOf(age).toCharArray();
+        for(int i = 0; i < ch.length; i++){
+            sb.append((char)(ch[i] - '0' + 'a'));
         }
-        return answer;
+        
+        return sb.toString();
     }
 }
