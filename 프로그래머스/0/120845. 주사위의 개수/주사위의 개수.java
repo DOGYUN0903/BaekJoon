c8fa6baac0f67dadf2b692c6c79efa637a4ae73b
DOGYUNKIM <kimdk0903@naver.com>
2025-09-19 10:47:54 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int answer = 0;
-        int x = box[0] / n;
-        int y = box[1] / n;
-        int z = box[2] / n;
-        return x*y*z;
+        int answer = 1;
+        for(int i = 0; i < box.length; i++){
+            answer *= box[i]/n;
+        }
+        return answer;
     }
 }
