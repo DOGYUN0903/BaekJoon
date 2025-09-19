eb1b5fed74fa595923f84c7f6a3438395adef7ac
DOGYUNKIM <kimdk0903@naver.com>
2025-09-19 11:36:52 +0900

    [level 0] Title: 약수 구하기, Time: 0.16 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int n) {
         List<Integer> lists = new ArrayList<>();
-        for (int i = 1; i <= n; i++){
+        for(int i = 1; i <= n; i++){
             if (n % i == 0){
                 lists.add(i);
             }
