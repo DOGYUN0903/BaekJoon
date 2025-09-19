b9188bcbcb80116023483df90ecbeb482376c76f
DOGYUNKIM <kimdk0903@naver.com>
2025-09-19 11:58:22 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.61 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,12 +2,18 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        my_string = my_string.toLowerCase();
+        StringBuilder sb = new StringBuilder();
         
         char[] ch = my_string.toCharArray();
         
-        Arrays.sort(ch);
+        for (int i = 0; i < ch.length; i++){
+            sb.append(Character.toLowerCase(ch[i]));
+        }
         
-        return String.valueOf(ch);
+        String st = sb.toString();
+        char[] c = st.toCharArray();
+        Arrays.sort(c);
+        
+        return String.valueOf(c);
     }
 }
