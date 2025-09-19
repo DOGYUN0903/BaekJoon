3a7299d942c0969b0fb8ce2e24fc3420609ceda7
DOGYUNKIM <kimdk0903@naver.com>
2025-09-19 13:29:37 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,14 +1,9 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        String num = String.valueOf(order);
-        char[] ch = num.toCharArray();
+        char[] ch = String.valueOf(order).toCharArray();
         for(int i = 0; i < ch.length; i++){
-            if ((ch[i] - '0') == 3){
-                answer++;
-            } else if ((ch[i] - '0') == 6) {
-                answer++;
-            } else if ((ch[i] - '0') == 9){
+            if (ch[i] - '0' == 3 || ch[i] - '0' ==6 || ch[i] - '0' == 9){
                 answer++;
             }
         }
