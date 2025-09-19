4b4d479deb5797ec281cdd010bfae5f48f06adb7
DOGYUNKIM <kimdk0903@naver.com>
2025-09-19 10:45:11 +0900

    [level 0] Title: 가위 바위 보, Time: 0.45 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,16 +1,18 @@
 class Solution {
     public String solution(String rsp) {
-        String answer = "";
-        char[] ch = rsp.toCharArray();
-        for(int i = 0; i < ch.length; i++){
-            if (ch[i] == '2'){
-                answer += "0";
-            } else if (ch[i] == '0'){
-                answer += "5";
-            } else if (ch[i] == '5'){
-                answer += "2";
+        StringBuilder sb = new StringBuilder();
+        
+        String[] s = rsp.split("");
+        
+        for (int i = 0; i < s.length; i++){
+            if (s[i].equals("2")){
+                sb.append("0");
+            } else if (s[i].equals("0")){
+                sb.append("5");
+            } else {
+                sb.append("2");
             }
         }
-        return answer;
+        return sb.toString();
     }
 }
