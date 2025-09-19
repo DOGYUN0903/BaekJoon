55863f80e41c398a497bf4d34a1ee9805489cb43
DOGYUNKIM <kimdk0903@naver.com>
2025-09-19 12:02:42 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int num, int k) {
-        String numbers = String.valueOf(num);
-        char[] ch = numbers.toCharArray();
+        char[] ch = String.valueOf(num).toCharArray();
         
         for(int i = 0; i < ch.length; i++){
             if (ch[i] - '0' == k){
-                return i + 1;
+                return i+1;
             }
         }
         return -1;
