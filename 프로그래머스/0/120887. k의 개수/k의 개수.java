0b456f1b488d71622049922af246478d7d416349
DOGYUNKIM <kimdk0903@naver.com>
2025-09-20 12:42:03 +0900

    [level 0] Title: k의 개수, Time: 25.74 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,14 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
+        String kStr = String.valueOf(k);
+        
         for (int z = i; z <= j; z++){
-            String numZ = String.valueOf(z);
-            for (int p = 0; p < numZ.length(); p++){
-                if (numZ.charAt(p) - '0' == k){
+            String num = String.valueOf(z);
+            
+            for (char c : num.toCharArray()){
+                if (c == kStr.charAt(0)){
                     answer++;
                 }
             }
         }
+        
         return answer;
     }
 }
