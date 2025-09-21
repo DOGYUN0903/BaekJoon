86b6a1cda21e7b525c299e0c62063df428b3d089
DOGYUNKIM <kimdk0903@naver.com>
2025-09-21 18:28:07 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,24 +1,25 @@
-import java.util.*;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for(int a = 0; a < commands.length; a++){
-            int i = commands[a][0] - 1;
-            int j = commands[a][1] - 1;
-            int k = commands[a][2] - 1;
-            
-            int[] arr = new int[j - i + 1];
-            
-            for(int b = i; b <= j; b++){
-                arr[b-i] = array[b];
+        for (int i = 0; i < answer.length; i++){     
+            List<Integer> lists = new ArrayList<>();
+            for (int j = commands[i][0] - 1; j <= commands[i][1] - 1; j++){
+                lists.add(array[j]);              
             }
             
-            Arrays.sort(arr);
+            int[] temp = new int[lists.size()];     
+            for (int k = 0; k < temp.length; k++){
+                temp[k] = lists.get(k);                
+            }
+            Arrays.sort(temp);
             
-            answer[a] = arr[k];
+            answer[i] = temp[commands[i][2] - 1];
         }
+        
         return answer;
     }
 }
