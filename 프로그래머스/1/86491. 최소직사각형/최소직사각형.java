0753749b2a073b668164df88e97aa6473de8062f
DOGYUNKIM <kimdk0903@naver.com>
2025-09-21 21:03:11 +0900

    [level 1] Title: 최소직사각형, Time: 1.85 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,15 +1,18 @@
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
     public int solution(int[][] sizes) {
         int maxWidth = 0;
-        int maxHeight = 0;
-
-        for (int[] card : sizes) {
-            int w = Math.max(card[0], card[1]);
-            int h = Math.min(card[0], card[1]);
-            maxWidth = Math.max(maxWidth, w);
-            maxHeight = Math.max(maxHeight, h);
+        int minWidth = 0;
+        
+        for (int[] size : sizes){
+            int bigger = Math.max(size[0], size[1]);
+            int smaller = Math.min(size[0], size[1]);
+            
+            maxWidth = Math.max(maxWidth, bigger);
+            minWidth = Math.max(minWidth, smaller);            
         }
 
-        return maxWidth * maxHeight;
+        return maxWidth * minWidth;
     }
 }
