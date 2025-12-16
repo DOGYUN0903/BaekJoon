29f54d29eb5f3e50323af0314f780694989c327b
DOGYUNKIM <kimdk0903@naver.com>
2025-12-16 13:41:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 102.81 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,24 @@
+import java.util.Map;
 import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         
         for (String name : participant){
-            if (map.containsKey(name)){
-                map.put(name, map.get(name) + 1);
-            } else {
-                map.put(name, 1);
-            }
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
         for (String name : completion){
-            map.put(name, map.get(name) -1);
+            map.put(name, map.get(name) -  1);
         }
         
-        for (String name : map.keySet()){
-            if (map.get(name) == 1){
-                return name;
+        for (String key : map.keySet()){
+            if (map.get(key) == 1){
+                return key;
             }
         }
         
-        return "";
+        return "0";
     }
 }
