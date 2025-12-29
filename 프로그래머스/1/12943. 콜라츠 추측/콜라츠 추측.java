8cc61d20718154d4aab7460986044ec5c79a0615
DOGYUNKIM <kimdk0903@naver.com>
2025-12-29 14:00:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,18 +1,22 @@
 class Solution {
     public int solution(int num) {
         int answer = 0;
-        long number = num;
-        if (number == 1) return 0;
-        while (number > 1){
-            if (number % 2 == 0){
-                number = number / 2;
-                answer++;
-            } else {
-                number = number * 3 + 1;
-                answer++;
+        long longNum = (long) num;
+        
+        while (longNum != 1){
+            
+            if (answer >= 500){
+                return -1;
             }
-            if (answer >= 500) return -1; 
-        } 
+            
+            if (longNum % 2 == 0){
+                longNum /= 2;
+            } else {
+                longNum = longNum * 3 + 1;
+            }
+            answer++;
+        }
+        
         return answer;
     }
 }
