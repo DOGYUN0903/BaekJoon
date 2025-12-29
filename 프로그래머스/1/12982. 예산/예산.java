4e137ca0cc6f031d60c006d065cff7ac17f9de7f
DOGYUNKIM <kimdk0903@naver.com>
2025-12-30 00:35:42 +0900

    [level 1] Title: 예산, Time: 0.49 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,18 +1,22 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
+        int sum = 0;
+        
         Arrays.sort(d);
         
         for (int i = 0; i < d.length; i++){
-            if(budget >= d[i]){
-                budget -= d[i];
-                answer++;
-            } else {
+            sum += d[i];
+            
+            if (sum > budget){
                 break;
             }
+            
+            answer++;
         }
+        
         return answer;
     }
 }
