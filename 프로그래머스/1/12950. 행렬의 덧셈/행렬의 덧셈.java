cfd18beea3e721f18ef519fa161d76f863bbca7b
DOGYUNKIM <kimdk0903@naver.com>
2025-12-29 19:22:00 +0900

    [level 1] Title: 행렬의 덧셈, Time: 5.07 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int row = arr1.length;
-        int col = arr1[0].length;
-        int[][] answer = new int[row][col];
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        
+        for (int i = 0; i < arr1.length; i++){
+            for (int j = 0; j < arr1[i].length; j++){
                 answer[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
+        
         return answer;
     }
 }
