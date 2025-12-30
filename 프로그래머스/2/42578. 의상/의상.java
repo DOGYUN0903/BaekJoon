d797009b60eff6d398b9ad032873f3286afdb8c1
DOGYUNKIM <kimdk0903@naver.com>
2025-12-30 16:00:22 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,23 +3,18 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(String[][] clothes) {
+        int answer = 0;
         Map<String, Integer> map = new HashMap<>();
         
-        for (String[] cloth : clothes){
-            String type = cloth[1];
-            
-            if (map.containsKey(type)){
-                map.put(type, map.get(type) + 1);
-            } else {
-                map.put(type, 1);
-            }
+        for (int i = 0; i < clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        int result = 1;
-        for (Integer count : map.values()){
-            result *= (count + 1);
+        int sum = 1;
+        for (String key : map.keySet()){
+            sum = sum * (map.get(key) + 1);
         }
         
-        return result - 1;
+        return sum - 1;
     }
 }
