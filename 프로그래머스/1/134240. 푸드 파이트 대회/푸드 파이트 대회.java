69c2ab7892ab0bf0325fc06cab9ce2ab221c1144
DOGYUNKIM <kimdk0903@naver.com>
2026-01-05 12:44:07 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 15.53 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,22 +1,23 @@
+
+// 1대1로 대결
+// 매 대결마다 음식의 종류와 양이 바뀜
+// 한 선수는 제일 왼쪽부터 오른쪽으로, 다른 선수는 제일 오른쪽부터 왼쪽으로(중앙에는 물이 있고, 물을 먼저 먹는 선수가 승리)
+// 두 선수가 먹는 음식의 종류와 양이 같아야함
+// 음식을 먹는 순서도 같아야함
+
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        int[] newFood = new int[food.length - 1];
-        for(int i = 0; i < newFood.length; i++){
-            newFood[i] = food[i+1] / 2;
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for(int j = 1; j <= newFood.length; j++){
-            for(int m = 0; m < newFood[j - 1]; m++){
-                sb.append(j);
+        StringBuilder left = new StringBuilder();
+
+        for (int i = 1; i < food.length; i++){
+            int count = food[i] / 2;
+            for (int j = 0; j < count; j++){
+                left.append(i);
             }
         }
         
-        StringBuilder sbReverse = new StringBuilder(sb).reverse();
-        
-        answer = sb.toString() + "0" + sbReverse.toString();
-        
-        return answer;
+        String leftStr = left.toString();
+        String rightStr = left.reverse().toString();
+        return leftStr + "0" + rightStr;
     }
 }
