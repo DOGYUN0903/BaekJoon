a6364e2f09ed754e743dc647bd9c98ea45cfabcd
DOGYUNKIM <kimdk0903@naver.com>
2026-01-08 12:49:10 +0900

    [level 2] Title: 가장 큰 수, Time: 215.88 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,24 +1,24 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
     public String solution(int[] numbers) {
+        String[] numStr = new String[numbers.length];
         
-        String[] str = new String[numbers.length];
-        for (int i = 0; i < str.length; i++){
-            str[i] = String.valueOf(numbers[i]);
+        for (int i = 0; i < numbers.length; i++){
+            numStr[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(str, new Comparator<String>() {
-            @Override
-            public int compare(String a, String b) {
-                return (b + a).compareTo(a + b);
-            }
+        Arrays.sort(numStr, (a, b) -> {
+            String ab = a + b;
+            String ba = b + a;
+            
+            return ba.compareTo(ab);
         });
         
         StringBuilder sb = new StringBuilder();
-        for (String s : str){
-            sb.append(s);
+        
+        for (int i = 0; i < numStr.length; i++){
+            sb.append(numStr[i]);
         }
         
         if (sb.toString().charAt(0) == '0') {
