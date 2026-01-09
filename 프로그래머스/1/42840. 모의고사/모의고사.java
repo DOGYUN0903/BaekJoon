da9ebcaa938648722d31add299a9a2bbedebe95f
DOGYUNKIM <kimdk0903@naver.com>
2026-01-09 15:38:59 +0900

    [level 1] Title: 모의고사, Time: 1.06 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,44 +3,40 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] answers) {
-        
         int[] num1 = {1,2,3,4,5};
         int[] num2 = {2,1,2,3,2,4,2,5};
         int[] num3 = {3,3,1,1,2,2,4,4,5,5};
-        
+
         int[] scores = new int[3];
         
-        for (int i = 0; i < answers.length; i++){
-            if(answers[i] == num1[i % num1.length]) {
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == num1[i % num1.length]) {
                 scores[0]++;
-            }
-            
-            if(answers[i] == num2[i % num2.length]) {
+            } if (answers[i] == num2[i % num2.length]) {
                 scores[1]++;
-            }
-            
-            if(answers[i] == num3[i % num3.length]) {
+            } if (answers[i] == num3[i % num3.length]) {
                 scores[2]++;
             }
         }
         
         int high = 0;
-        for (int score : scores){
+        for (int score : scores) {
             high = Math.max(high, score);
         }
         
-        List<Integer> lists = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < scores.length; i++){
-            if (scores[i] == high){
-                lists.add(i + 1);
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == high) {
+                list.add(i + 1);
             }
         }
-    
-        int[] answer = new int[lists.size()];
-        for (int i = 0; i < answer.length; i++){
-            answer[i] = lists.get(i);
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
         }
+        
         return answer;
     }
 }
