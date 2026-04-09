c3b4a9142172f7d134be226c15e5fa9a376b44ba
DOGYUNKIM <kimdk0903@naver.com>
2026-04-09 13:42:24 +0900

    [Bronze III] Title: 윷놀이, Time: 176 ms, Memory: 17632 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.java
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++){
             int[] yut = new int[4];
-            for (int j = 0; j < 4; j++) {
+            for (int j = 0; j < 4; j++){
                 yut[j] = sc.nextInt();
             }
             
             int count = 0;
-            for (int val : yut) {
-                if (val == 0) count++;
+            for (int num : yut) {
+                if (num == 1) count++;
             }
             
             if (count == 0) {
-                System.out.println("E");
+                System.out.println("D");
             } else if (count == 1) {
-                System.out.println("A");
+                System.out.println("C");
             } else if (count == 2) {
                 System.out.println("B");
             } else if (count == 3) {
-                System.out.println("C");
+                System.out.println("A");
             } else {
-                System.out.println("D");
+                System.out.println("E");
             }
         }
     }
