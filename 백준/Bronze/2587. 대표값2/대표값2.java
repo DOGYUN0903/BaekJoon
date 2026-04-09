2adbefff61f723ef49000934192422aae5d31845
DOGYUNKIM <kimdk0903@naver.com>
2026-04-09 14:08:52 +0900

    [Bronze II] Title: 대표값2, Time: 172 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,19 +1,22 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sum = 0;
-        int mid = 0;
-        int[] arr = new int[5];
-        for (int i = 0; i < 5; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] numArr = new int[5];
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
-        int avg = sum / 5;
-        mid = arr[2];
-        System.out.print(avg + "\n" + mid);
+        
+        Arrays.sort(numArr);
+        
+        int sum = 0;
+        for (int num : numArr) {
+            sum += num;
+        }
+        
+        System.out.println(sum/5);
+        System.out.println(numArr[2]);
     }
 }
