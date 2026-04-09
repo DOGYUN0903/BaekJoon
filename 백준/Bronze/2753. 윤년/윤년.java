a6973ec090485cf9d08418162dc94380f5cff44b
DOGYUNKIM <kimdk0903@naver.com>
2026-04-09 12:21:19 +0900

    [Bronze V] Title: 윤년, Time: 176 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,22 +1,17 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int Year = sc.nextInt();
+        int year = sc.nextInt();
         
-        if(Year%4==0){
-            if(Year%400==0){
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0){
                 System.out.println("1");
-            }
-            else if(Year%100==0){
+            } else {
                 System.out.println("0");
             }
-            else{
-                System.out.println("1");
-            }
-        }
-        else{
+        } else {
             System.out.println("0");
         }
     }
