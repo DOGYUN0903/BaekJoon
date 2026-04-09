fe12c40fa10f691d28c503054dfd8bfc457d048b
DOGYUNKIM <kimdk0903@naver.com>
2026-04-09 13:25:56 +0900

    [Bronze IV] Title: 주사위 세개, Time: 168 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,49 +1,19 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int[] counts = new int[7]; // 주사위 눈의 수 카운트 배열
-
-        // 주사위 눈의 수 입력받기
-        for (int i = 0; i < 3; i++) {
-            int num = scanner.nextInt();
-            counts[num]++;
+        Scanner sc = new Scanner(System.in);
+        int[] dice = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+        
+        Arrays.sort(dice);
+        
+        if (dice[0] == dice[2]) {
+            System.out.println(10000 + dice[0] * 1000);
+        } else if (dice[0] == dice[1] || dice[1] == dice[2]) {
+            System.out.println(1000 + dice[1] * 100);
+        } else {
+            System.out.println(dice[2] * 100);
         }
-
-        int result = 0;
-
-        // 세 개의 주사위 눈이 같은 경우
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] == 3) {
-                result = 10000 + i * 1000;
-                break;
-            }
-        }
-
-        // 두 개의 주사위 눈이 같은 경우
-        if (result == 0) {
-            for (int i = 1; i <= 6; i++) {
-                if (counts[i] == 2) {
-                    result = 1000 + i * 100;
-                    break;
-                }
-            }
-        }
-
-        // 모두 다른 눈의 수인 경우
-        if (result == 0) {
-            for (int i = 6; i >= 1; i--) {
-                if (counts[i] == 1) {
-                    result = i * 100;
-                    break;
-                }
-            }
-        }
-
-        System.out.println(result);
-
-        scanner.close();
     }
 }
