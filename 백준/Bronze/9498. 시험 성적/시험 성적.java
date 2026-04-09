330ba4d1478e540436e463a71091eb23da22dbbc
DOGYUNKIM <kimdk0903@naver.com>
2026-04-09 12:09:04 +0900

    [Bronze V] Title: 시험 성적, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        if(A>=90){
+        int score = sc.nextInt();
+        
+        if (score >= 90) {
             System.out.println("A");
-        } else if(A>=80){
+        } else if (score >= 80 && score < 90) {
             System.out.println("B");
-        } else if(A>=70){
+        } else if (score >= 70 && score < 80) {
             System.out.println("C");
-        } else if(A>=60){
+        } else if (score >= 60 && score < 70) {
             System.out.println("D");
         } else {
             System.out.println("F");
